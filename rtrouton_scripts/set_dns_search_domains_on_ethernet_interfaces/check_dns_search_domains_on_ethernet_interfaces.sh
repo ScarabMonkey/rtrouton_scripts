e1676527315e55a8e91bb78909efa6e0c98aa6ec
Merlin Hartley <merlinhartley@hotmail.com>
2017-11-22 11:37:47 +0000

    Ready for munki

---
+++ rtrouton_scripts/set_dns_search_domains_on_ethernet_interfaces/check_dns_search_domains_on_ethernet_interfaces.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# For the SearchDomains variable, change the search domains
+# to be the ones you need, separated by spaces. For example,
+# if you needed to set verizon.com and comcast.com as DNS
+# search domains, SearchDomains would be set like this:
+#
+# SearchDomains="verizon.com comcast.com"
+#
+
+SearchDomains="mrc-mbu.cam.ac.uk ad.mrc-mbu.cam.ac.uk servers.mrc-mbu.cam.ac.uk"
+
+
+# Have the Mac scan for and register
+# any new network hardware that has
+# not already registered.
+
+#/usr/sbin/networksetup -detectnewhardware
+
+# Save current IFS state
+
+OLDIFS=$IFS
+
+# Change IFS to
+# create newline
+
+IFS=$'\n'
+
+# read all Ethernet interface names into an array
+
+ethernet_interface=($(networksetup -listallnetworkservices | awk '/Ethernet/'))
+
+# restore IFS to previous state
+
+IFS=$OLDIFS
+
+# Get length of the array
+
+tLen=${#ethernet_interface[@]}
+
+# Loops through the list of Ethernet network interfaces
+# available on this Mac and sets the specified DNS search
+# domains on each Ethernet network interface.
+
+for (( i=0; i<${tLen}; i++ ));
+  do
+    #/bin/echo "Getting SearchDomains for "${ethernet_interface[$i]}" on this Mac."
+    #/usr/sbin/networksetup -getsearchdomains "${ethernet_interface[$i]}"
+    /usr/sbin/networksetup -getsearchdomains "${ethernet_interface[$i]}" | grep "ad.mrc-mbu.cam.ac.uk"
+  done
